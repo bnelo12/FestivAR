e668f015130923659631dde43895bbd6e6f181d5
Yu Jo <zoey5538@gmail.com>
2019-03-16 14:53:32 +0000

    Get unique list of venues

--- festival_data.py
+++ festival_data.py
@@ -5,10 +5,14 @@ import json
 
 api_key     = 'txzPDI2zALPED8Aw'
 secret_key  = 'dBxAEbaR5Od5hAv2BpabFlaaaAC1QoLM'
-query       = '/events?festival=science&key=' + api_key
+query       = '/events?key=' + api_key
 signature   = hmac.new(bytearray(secret_key, 'utf-8'), bytearray(query, 'utf-8'), hashlib.sha1).hexdigest()
 url         = 'https://api.edinburghfestivalcity.com' + query + '&signature=' + signature
 response = requests.get(url)
 data = response.text
 parsed = json.loads(data)
-print(json.dumps(parsed, indent=4))
+
+# name, lat, lon
+venues = [[event['venue']['name'], event['venue']['position']['lat'], event['venue']['position']['lon']] for event in parsed]
+unique_venues = [list(x) for x in set(tuple(x) for x in venues)]
+print(unique_venues)
