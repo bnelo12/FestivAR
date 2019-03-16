a5ddc7a90837091c150460e634bccc18885cdca8
Yu Jo <zoey5538@gmail.com>
2019-03-16 16:40:21 +0000

    Get event info

--- festival_data.py
+++ festival_data.py
@@ -12,7 +12,27 @@ response = requests.get(url)
 data = response.text
 parsed = json.loads(data)
 
-# name, lat, lon
-venues = [[event['venue']['name'], event['venue']['position']['lat'], event['venue']['position']['lon']] for event in parsed]
+# Venues (list): name, lat, lon, wheelchair_access
+venues = [[event['venue']['name'], event['venue']['position']['lat'], event['venue']['position']['lon'], event['performance_space']['wheelchair_access']] for event in parsed]
 unique_venues = [list(x) for x in set(tuple(x) for x in venues)]
-print(unique_venues)
+
+# Events (dict): key - venue_name, value: list of event info (dict)
+events = {}
+for venue in venues:
+    venue_name = venue[0]
+    for event in parsed:
+        if event['venue']['name'] == venue_name:
+            info = {}
+            info['title'] = event['title']
+            info['artist'] = event['artist']
+            info['description_teaser'] = event['description_teaser']
+            info['discounts'] = event['discounts']
+            info['festival'] = event['festival']
+            info['genre_tags'] = event['genre_tags']
+            info['images'] = event['images']
+            info['wheelchair_access'] = event['performance_space']['wheelchair_access']
+            info['disabled_description'] = event['venue']['disabled_description']
+            info['next_performance'] = event['performances'][0]
+            events.setdefault(venue_name, []).append(info)
+
+print(events['Summerhall'])
